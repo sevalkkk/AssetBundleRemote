74732d24abbf4c4a19ee0cb6c4061029414121e1
sevalkkk <sevalnazkarahan@gmail.com>
2022-10-20 17:37:13 -0700

    sevalkkk

---
+++ assetbundleremote/Assets/Loader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    private AssetBundleCreateRequest bundleRequest;
+    private UnityWebRequest request;
+
+    private void Start()
+    {
+        request = UnityWebRequestAssetBundle.GetAssetBundle("https://drive.google.com/uc?export=download&id=1KDHT2tnOIrht8ucJN0HUnTQP0yaqgt23");
+        request.SendWebRequest();
+        print("indirildi");
+    }
+
+    private void Update()
+    {
+        if (request.isDone)
+        {
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+            SceneManager.LoadScene("level1");
+        }
+    }
+}
